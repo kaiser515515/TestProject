42fa23fdba812ddc7e3f8f8b69d8dc5d33b9bce6
kaiser <kaiser@DESKTOP-FA31S26.fios-router.home>
2019-05-12 23:59:08 -0400

    added new code

--- GitTestProject/src/git/testing/TestProject.java
+++ GitTestProject/src/git/testing/TestProject.java
@@ -4,7 +4,8 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		System.out.println("first github testing");
-
+        System.out.println("first push compeleted");
+		
 	}
 
 }
